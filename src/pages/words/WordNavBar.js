ffad586eab5fabd0f3ea972791d99f2a5a3739d4
DevelopedBy-Siva <developedby.siva@gmail.com>
2023-01-13 13:32:11 +0530

    Add search component for the words module

--- src/pages/words/WordNavBar.js
+++ src/pages/words/WordNavBar.js
@@ -1,21 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { BiSearch } from "react-icons/bi";
 import { BsPlusLg } from "react-icons/bs";
+import { IoCloseSharp } from "react-icons/io5";
 
 import NewWord from "./NewWord";
 
 export default function WordNavBar() {
+  const searchInputRef = useRef(null);
+
   const [addBtnActive, setAddBtnActive] = useState(null);
+  const [searchInput, setSearchInput] = useState("");
+  const [searchActive, setSearchActive] = useState(false);
 
   const toggleAddWord = (opt = null) => setAddBtnActive(opt);
 
+  function handleWordInputChange(e) {
+    const value = e.target.value;
+    if (value.length > 0) {
+      const regex = /^[a-zA-Z]+$/;
+      if (!regex.test(value)) return;
+    }
+    setSearchInput(value);
+  }
+
+  useEffect(() => {
+    if (searchInputRef.current) searchInputRef.current.focus();
+  }, [searchActive]);
+
   return (
     <Container>
-      <SearchBtn>
-        <BiSearch />
-      </SearchBtn>
+      <SearchBox>
+        <AnimatePresence>
+          {searchActive && (
+            <SearchInputBox
+              ref={searchInputRef}
+              type="text"
+              spellCheck="false"
+              inputMode="search"
+              placeholder="Search word here..."
+              value={searchInput}
+              onChange={handleWordInputChange}
+              animate={{ width: searchActive ? "240px" : 0 }}
+              exit={{
+                width: 0,
+                opacity: 0,
+                transition: {
+                  duration: 0.3,
+                },
+              }}
+              transition={{ duration: 0.5 }}
+            />
+          )}
+        </AnimatePresence>
+        <SearchBtn onClick={() => setSearchActive(!searchActive)}>
+          {searchActive ? <IoCloseSharp /> : <BiSearch />}
+        </SearchBtn>
+      </SearchBox>
       <NewBtn onClick={() => toggleAddWord(true)}>
         <BsPlusLg />
         <NewBtnLabel>New</NewBtnLabel>
@@ -30,32 +72,22 @@ export default function WordNavBar() {
 const Container = styled.div`
   display: flex;
   align-items: center;
-`;
-
-const SearchBtn = styled.button`
-  color: ${(props) => props.theme.text.dull};
-  border: none;
-  outline: none;
-  background: none;
-  font-size: 1.6rem;
-  display: flex;
-  align-items: center;
-  padding: 0px;
-  border-radius: 4px;
-  cursor: pointer;
+  justify-content: flex-end;
+  width: 100%;
 `;
 
 const NewBtn = styled.button`
   background: ${(props) => props.theme.button.green};
+  border: 1px solid ${(props) => props.theme.button.green};
   color: ${(props) => props.theme.text.light};
   display: flex;
   align-items: center;
   justify-content: space-around;
-  border: none;
   outline: none;
   margin-left: 10px;
   font-size: 0.7rem;
-  padding: 7px 18px;
+  height: 30px;
+  padding: 0 18px;
   border-radius: 5px;
   cursor: pointer;
 `;
@@ -64,4 +96,39 @@ const NewBtnLabel = styled.span`
   font-size: 0.8rem;
   letter-spacing: 1px;
   margin-left: 4px;
+`;
+
+const SearchBox = styled(motion.div)`
+  position: relative;
+  padding: 0 7px;
+  border-radius: 5px;
+  height: 30px;
+  border: 1px solid ${(props) => props.theme.button.dull};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SearchBtn = styled.button`
+  color: ${(props) => props.theme.text.dull};
+  border: none;
+  outline: none;
+  background: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  font-size: 1.2rem;
+  float: right;
+`;
+
+const SearchInputBox = styled(motion.input)`
+  max-width: 90%;
+  width: 0;
+  border: none;
+  outline: none;
+  background: none;
+  color: ${(props) => props.theme.text.dull};
+  font-size: 0.8rem;
+  float: left;
 `;
