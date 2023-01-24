6d746571f350a4fbe10a2fa5cc75a8ca1b036d5a
DevelopedBy-Siva <siva@Sivasankers-MacBook-Pro.local>
2023-01-24 15:45:35 +0530

    Initial redux store before page re-renders

--- src/pages/quiz/index.js
+++ src/pages/quiz/index.js
@@ -1,22 +1,15 @@
-import React, { lazy, Suspense, useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { lazy, Suspense, useState } from "react";
+import { useSelector } from "react-redux";
 
 import Wrapper from "../../components/wrapper";
 import Header from "../../components/header";
 import QuizFormContainer from "./QuizFormContainer";
 import QuizNavContainer from "./QuizNavContainer";
-import { initialiseWords } from "../../redux/reducer/Words";
 
 const QnAnsContainer = lazy(() => import("./QnAnsContainer"));
 
 export default function Quiz() {
   const [quizQn, setQuizQn] = useState(null);
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(initialiseWords());
-  }, [dispatch]);
-
   const words = useSelector((state) => state.words.words);
 
   return (
--- src/pages/words/WordsWrapper.js
+++ src/pages/words/WordsWrapper.js
@@ -1,18 +1,11 @@
-import React, { lazy, Suspense, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { lazy, Suspense } from "react";
+import { useSelector } from "react-redux";
 
 import Indicators from "./Indicators";
-import { initialiseWords } from "../../redux/reducer/Words";
 
 const WordsContainer = lazy(() => import("./WordsContainer"));
 
 export default function WordsWrapper() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(initialiseWords());
-  }, [dispatch]);
-
   const initializing = useSelector((state) => state.words.loading);
 
   if (initializing) return <Indicators loading />;
